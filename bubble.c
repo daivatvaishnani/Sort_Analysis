b2cb9ed0ce6da114f6fba5ee90bb1b98e67e8df8
Daivat Vaishnani <daivatvaishnani18@gmail.com>
2016-12-14 00:53:27 +0530

    bubble_sort algorith (*.c)

---
+++ bubble.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+#include "sorting.c"
+#include<stdlib.h>
+
+void main()
+{
+  int n,i,counter;  //count will have the count of number of comparisons
+  scanf("%d",&n);
+
+  int *A = malloc(sizeof(int)*n);
+  for(i=0; i<n;i++)
+   {
+     scanf("%d",&A[i]);
+   }
+
+  
+   counter = bubble(A,n);
+
+   printf("%d \n",counter);
+
+   return;
+
+}
